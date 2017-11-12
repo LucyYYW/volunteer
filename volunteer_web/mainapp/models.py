6001d61872b0d6ec962754662ae8e01672aacde5
LUCY-PC\Lucy <yuewei5913@sina.com>
2017-11-12 09:22:50 +0800

    no message

--- volunteer_web/mainapp/models.py
+++ volunteer_web/mainapp/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+	name = models.CharField()
+	number = models.CharField(max_length=8)
+	password = models.CharField(max_length=8)
+
+	male = 'M'
+	female = 'F'
+	sex_choices = (
+		(male, 'male')
+		(female, 'female')
+	)
+	sex = models.CharField(
+		max_length=2,
+		choices = sex_choices,
+		default = male
+	)
+
+	hours_got = models.IntergerField()
+	misconduct = models.BooleanField()
+	GPA_ranking = models.IntergerField()
+
+
+
+class Student_job(models.Model):
+	student = models.ForeignKey('Student')
+	job = models.ForeignKey('Job')
+
+	Applied = 1
+	Approved = 2
+	Unapproved = 3
+	Finished = 4
+	status_choices = (
+		(Applied, "applied"),
+		(Approved, "approved"),
+		(Unapproved, 'unapproved'),
+		(Finished, "finished")
+	)
+	status = models.IntergerField(
+		choices = status_choices,
+		default = Applied
+	)
+
+	written_info = models.CharField(max_length = 200)
+
+
+
+class Teacher(models.Model):
+	user = models.CharField()
+	password = models.CharField(max_length=8)
+
+
+class Club(models.Model):
+	name = models.CharField()
+	number = models.CharField(max_length=8)
+	password = models.CharField(max_length=8)
+
+
+class Job(models.Model):
+	club = models.ForeignKey('Club')
+	title = models.CharField()
+	location = models.CharField()
+	time = models.DateTimeField()
+	hours = models.IntergerField()
+	deadline = models.DateTimeField()
+	additional_info = models.CharField(max_length=200)
+	whether_approved = models.BooleanField(default=True)
--- volunteer_web/volunteer_web/settings.py
+++ volunteer_web/volunteer_web/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'volunteer_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
